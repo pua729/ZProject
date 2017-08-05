8c6080a72829041beb2ab2a06e8ef772a6f65435
tsune <std0dc00@gmail.com>
2017-08-05 12:44:35 +0900

    add TestShader scene

--- Assets/Scripts/Common/GameScenes.cs
+++ Assets/Scripts/Common/GameScenes.cs
@@ -8,5 +8,6 @@ namespace Common
 	{
 		Intro,
 		Test,
+		ShaderTest,
 	}
 }
--- Assets/Scripts/Intro/Controller.cs
+++ Assets/Scripts/Intro/Controller.cs
@@ -7,15 +7,22 @@ namespace Intro.ViewModels
 	public class Controller : MonoBehaviour
 	{
 		public Button unitychanUiButton;
+		public Button shaderTestUiButton;
 
 		public void Start()
 		{
 			unitychanUiButton.onClick.AddListener(onClickUnitychanUiButton);
+			shaderTestUiButton.onClick.AddListener(onClickShaderTestUiButton);
 		}
 
 		public void onClickUnitychanUiButton()
 		{
 			SceneManager.LoadScene(Common.GamesScenes.Test.ToString());
+		}
+
+		public void onClickShaderTestUiButton()
+		{
+			SceneManager.LoadScene(Common.GamesScenes.ShaderTest.ToString());
 		}
 	}
 }
